7e9dd7d0637bb9f80272f9021094c38b7e11ed3b
tiago-sb <belatiago72@gmail.com>
2024-03-21 16:11:12 -0300

    adiciona código da implementação da lista encadeada

---
+++ src/aed_unidade1/lista_encadeada/ListaEncadeada.java
@@ -0,0 +1,109 @@
+package aed_unidade1.lista_encadeada;
+
+public class ListaEncadeada {
+    private Element head;
+    private Element tail;
+    
+    public ListaEncadeada() {}
+    
+    public class Element {
+    	Object data;
+    	Element next;
+    	
+    	public Element(Object data, Element next) {
+    		this.data = data;
+    		this.next = next;
+    	}
+    	
+    	public Element(){}
+    	
+    	public Object getData() {
+    		return this.data;
+    	}
+    	
+    	public Element getNext() {
+    		return this.next;
+    	}
+    }
+    
+    
+    public Object getHeadValue() throws Exception {
+        if (head == null){
+            throw new Exception();
+        }
+
+        return head.data;
+    }
+    
+    public Object getTailValue() throws Exception {
+        if (tail == null){
+            throw new Exception();
+        }
+
+        return tail.data;
+    }
+    
+    
+    public boolean estaVazia(){
+        return head == null; 
+    }
+    
+    public void fazVazia() {
+        this.head = null;
+        this.tail = null;
+    }
+    
+    public void inserirInicio(Object item){
+        Element temporaria = new Element (item, head);
+        if (head == null){
+            tail = temporaria;
+        }
+     
+        head = temporaria;
+    }
+    
+    public void inserirFim(Object item){
+        Element temporaria = new Element(item, null);
+        if (head == null){
+            head = temporaria;
+        } else {
+            tail.next = temporaria;
+        }
+        
+        tail = temporaria;
+    }
+    
+    public void atribuir (ListaEncadeada elemento){
+        if (elemento != this){
+           this.fazVazia();
+            for (Element ptr = elemento.head; ptr != null; ptr = ptr.next){
+                this.inserirFim(ptr.data);
+            }
+        }
+    }
+    
+    public void extrair(Object item) throws Exception {
+        Element ptr = head;
+        Element prevPtr = null;
+    
+        while (ptr != null && ptr.data!= item){
+            prevPtr = ptr;
+            ptr = ptr.next; 
+        }
+    
+        if (ptr == null){
+            throw new Exception();
+        }
+    
+        if (ptr == head){
+            head = ptr.next;
+        } else {
+            prevPtr.next = ptr.next;
+        }
+    
+        if (ptr == tail){ 
+            tail = prevPtr; 
+        }
+    }
+}
+
---
+++ src/aed_unidade1/lista_encadeada/Principal.java
@@ -0,0 +1,26 @@
+package aed_unidade1.lista_encadeada;
+
+public class Principal {
+	public static void main(String[] args) throws Exception {
+		ListaEncadeada lista = new ListaEncadeada();
+		
+		// adicionar tiago e anna na lista
+		lista.inserirInicio("tiago");
+		lista.inserirFim("anna");
+		
+		// testar se a lista esta vazia ou nao
+		// caso esteja, o resultado esperado eh true, caso nao, o resultado esperado eh false
+		System.out.println(lista.estaVazia());
+		
+		// metodo para buscar o elemento que se encontra no head da lista
+		System.out.println(lista.getHeadValue());
+		
+		// metodo para buscar o elemento que se encontra na calda da lista
+		System.out.println(lista.getTailValue());
+		
+		// extrair o nome anna da lista
+		lista.extrair("anna");
+		
+		System.out.println(lista.getTailValue());
+	}
+}
