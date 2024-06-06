2a0619ae7be81d516782e6c9936e5f72f7921dd0
Tiago Santos Bela <belatiago72@gmail.com>
2024-06-06 12:28:10 -0300

    Add files via upload

--- src/aed_unidade3/Principal.java
+++ src/aed_unidade3/Principal.java
@@ -1,25 +1,26 @@
+// import algoritmosSort.BubbleSort;
+// import algoritmosSort.InsertionSort;
+import algoritmosSort.SelectionSort;
+
 public class Principal {
     public static void main(String[] args) {
-        int quantidade = 100000;
-        int[] vetor = new int[quantidade];
+        int[] vetor = new int[100000];
         
-        
-
         for (int i = 0; i < vetor.length; i++) {
-         vetor[i] = (int) (Math.random() * quantidade);
+            vetor[i] = (int) (Math.random() * 100000);
         }
         
         System.out.println("Vetor gerado!!!");
         
         // BubbleSort bS = new BubbleSort(vetor);
-        // SelectionSort sS = new SelectionSort(vetor);
-        InsertionSort iS = new InsertionSort(vetor);
+        SelectionSort sS = new SelectionSort(vetor);
+        // InsertionSort iS = new InsertionSort(vetor);
 
         long tempoInicial = System.currentTimeMillis();
         
         // bS.bubbleSort();
-        // sS.selectionSort();
-        iS.insertionSort();
+        sS.selectionSort();
+        // iS.insertionSort();
 
         long tempoFinal = System.currentTimeMillis();
         System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
